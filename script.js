65198d8fd2e5277341bb2d2cc4e22633857d42a0
abhiavi7 <118717274+abhiavi7@users.noreply.github.com>
2023-12-29 19:57:31 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,56 +1,104 @@
-//your JS code here.
+document.addEventListener("DOMContentLoaded", function () {
+    // Questions data
+    const questions = [
+        {
+            question: "What is the capital of France?",
+            choices: ["Paris", "London", "Berlin", "Madrid"],
+            answer: "Paris"
+        },
+        {
+            question: "What is the highest mountain in the world?",
+            choices: ["Everest", "Kilimanjaro", "Denali", "Matterhorn"],
+            answer: "Everest"
+        },
+        {
+            question: "What is the largest country by area?",
+            choices: ["Russia", "China", "Canada", "United States"],
+            answer: "Russia"
+        },
+        {
+            question: "Which is the largest planet in our solar system?",
+            choices: ["Earth", "Jupiter", "Mars"],
+            answer: "Jupiter"
+        },
+        {
+            question: "What is the capital of Canada?",
+            choices: ["Toronto", "Montreal", "Vancouver", "Ottawa"],
+            answer: "Ottawa"
+        },
+    ];
 
-// Do not change code below this line
-// This code will just display the questions to the screen
-const questions = [
-  {
-    question: "What is the capital of France?",
-    choices: ["Paris", "London", "Berlin", "Madrid"],
-    answer: "Paris",
-  },
-  {
-    question: "What is the highest mountain in the world?",
-    choices: ["Everest", "Kilimanjaro", "Denali", "Matterhorn"],
-    answer: "Everest",
-  },
-  {
-    question: "What is the largest country by area?",
-    choices: ["Russia", "China", "Canada", "United States"],
-    answer: "Russia",
-  },
-  {
-    question: "Which is the largest planet in our solar system?",
-    choices: ["Earth", "Jupiter", "Mars"],
-    answer: "Jupiter",
-  },
-  {
-    question: "What is the capital of Canada?",
-    choices: ["Toronto", "Montreal", "Vancouver", "Ottawa"],
-    answer: "Ottawa",
-  },
-];
+    // Reference to HTML elements
+    const questionsContainer = document.getElementById('questions');
+    const submitButton = document.getElementById('submit');
+    const scoreContainer = document.getElementById('score');
 
-// Display the quiz questions and choices
-function renderQuestions() {
-  for (let i = 0; i < questions.length; i++) {
-    const question = questions[i];
-    const questionElement = document.createElement("div");
-    const questionText = document.createTextNode(question.question);
-    questionElement.appendChild(questionText);
-    for (let j = 0; j < question.choices.length; j++) {
-      const choice = question.choices[j];
-      const choiceElement = document.createElement("input");
-      choiceElement.setAttribute("type", "radio");
-      choiceElement.setAttribute("name", `question-${i}`);
-      choiceElement.setAttribute("value", choice);
-      if (userAnswers[i] === choice) {
-        choiceElement.setAttribute("checked", true);
-      }
-      const choiceText = document.createTextNode(choice);
-      questionElement.appendChild(choiceElement);
-      questionElement.appendChild(choiceText);
+    // Load saved progress from session storage
+    const savedProgress = JSON.parse(sessionStorage.getItem('progress')) || {};
+
+    // Display the quiz questions and choices
+    renderQuestions();
+
+    // Add event listener to the submit button
+    submitButton.addEventListener('click', function () {
+        // Calculate and display the score
+        const score = calculateScore();
+        displayScore(score);
+
+        // Save score in local storage
+        localStorage.setItem('score', score);
+    });
+
+    // Function to display quiz questions
+    function renderQuestions() {
+        questions.forEach(function (question, index) {
+            const questionElement = document.createElement("div");
+            const questionText = document.createTextNode(question.question);
+            questionElement.appendChild(questionText);
+
+            question.choices.forEach(function (choice) {
+                const choiceElement = document.createElement("input");
+                choiceElement.setAttribute("type", "radio");
+                choiceElement.setAttribute("name", `question-${index}`);
+                choiceElement.setAttribute("value", choice);
+
+                // Check if the choice matches the saved progress
+                if (savedProgress[index] === choice) {
+                    choiceElement.setAttribute("checked", true);
+                }
+
+                const choiceText = document.createTextNode(choice);
+
+                questionElement.appendChild(choiceElement);
+                questionElement.appendChild(choiceText);
+            });
+
+            questionsContainer.appendChild(questionElement);
+        });
     }
-    questionsElement.appendChild(questionElement);
-  }
-}
-renderQuestions();
+
+    // Function to calculate the quiz score
+    function calculateScore() {
+        let score = 0;
+
+        questions.forEach(function (question, index) {
+            const selectedOption = document.querySelector(`input[name="question-${index}"]:checked`);
+
+            if (selectedOption && selectedOption.value === question.answer) {
+                score++;
+            }
+
+            // Save user's progress in session storage
+            savedProgress[index] = selectedOption ? selectedOption.value : null;
+            sessionStorage.setItem('progress', JSON.stringify(savedProgress));
+        });
+
+        return score;
+    }
+
+    // Function to display the quiz score
+    function displayScore(score) {
+        scoreContainer.innerHTML = `Your Score is ${score} out of 5.`;
+    }
+});
+
